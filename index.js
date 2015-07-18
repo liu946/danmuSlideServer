da4bfff0628c14aeda7c746227f09f857410544e
liu946 <liuyang570926881@gmail.com>
2015-07-19 00:54:27 +0800

    配置资源及路由

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express')
+var app = express()
+
+app.locals.title = 'Danmu Slide Show';
+app.locals.email = 'liuyang570926881@gmail.com';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/pages/')
 });
 
+app.get('/danmu/:id', function (req, res, next) {
+  console.log('Request page [danmu] :', req.params.id);
+  res.render('layouts/danmu', { slidefilename: req.params.id});
+});
+
+app.get('/slide/:id', function (req, res, next) {
+  console.log('Request page [slide] :', req.params.id);
+  res.render('pages/'+req.params.id);
+});
+app.set('view engine', 'jade');
+app.set('views', './views');
 app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
