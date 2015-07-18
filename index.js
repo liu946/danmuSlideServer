08301d32bb035779d66d4af66a3183785c036e19
liu946 <liuyang570926881@gmail.com>
2015-07-19 01:23:25 +0800

    server install & require socket io

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 var express = require('express')
 var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+// config
 app.locals.title = 'Danmu Slide Show';
 app.locals.email = 'liuyang570926881@gmail.com';
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/pages/')
-});
-
-app.get('/danmu/:id', function (req, res, next) {
-  console.log('Request page [danmu] :', req.params.id);
-  res.render('layouts/danmu', { slidefilename: req.params.id});
-});
-
-app.get('/slide/:id', function (req, res, next) {
-  console.log('Request page [slide] :', req.params.id);
-  res.render('pages/'+req.params.id);
-});
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
 
+// router
+app.get('/', function(req, res){
+  res.render('slidelist',{slides:['2015-07-16-example']});
+});
+app.get('/danmu/:id', function (req, res, next) {
+  console.log('Request page [danmu] :', req.params.id);
+  res.render('layouts/danmu', { slidefilename: req.params.id});
+});
+app.get('/slide/:id', function (req, res, next) {
+  console.log('Request page [slide] :', req.params.id);
+  res.render('pages/'+req.params.id);
+});
+
+// socket io
+var status = {'onlinenum':0,'danmunum':0}
+io.on('connection',function(socket){
+	console.log('[user connected]:'+(status['onlinenum']++));
+});
+
+
+// server
 var server = app.listen(3000, function () {
 
   var host = server.address().address
